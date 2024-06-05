7bc73d77db3f2cd9551ea2710de8d8c80af2fac9
Thamer Sekhri <95574184+T2ABIZZY@users.noreply.github.com>
2024-06-05 10:01:41 +0100

    showing all ranks

--- webscarping_ranking.py
+++ webscarping_ranking.py
@@ -18,10 +18,45 @@ accept_button = WebDriverWait(driver, 10).until(
     )
 accept_button.click()
 time.sleep(3)
-driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-button = WebDriverWait(driver, 10).until(
-        EC.element_to_be_clickable((By.CLASS_NAME,"button-module_contentContainer__QyE5V"))
-    )
-button.click()
+
+def is_element_in_view(driver, element):
+    return driver.execute_script("return (window.innerHeight + window.scrollY) >= arguments[0].getBoundingClientRect().bottom;", element)
+
+# Find the target button element
+target = WebDriverWait(driver, 10).until(
+    EC.presence_of_element_located((By.CLASS_NAME, "button-module_contentContainer__QyE5V"))
+)
+
+# Scroll incrementally until the target element is in view
+scroll_pause_time = 0.5  # Time to wait between scrolls
+
+while True:
+
+    driver.execute_script("window.scrollBy(0, 500);")
+    time.sleep(scroll_pause_time)
+
+    if is_element_in_view(driver, target):
+        driver.execute_script("arguments[0].scrollIntoView(true);", target)
+        break
+
+target = WebDriverWait(driver, 10).until(
+    EC.element_to_be_clickable((By.CLASS_NAME, "button-module_contentContainer__QyE5V"))
+)
+
+def is_target_element_present(driver):
+    elements = driver.find_elements(By.CLASS_NAME, "rank-cell_rank__yNDOI")
+    for element in elements:
+        if element.text == "210":
+            return True
+    return False
+    
+driver.execute_script("arguments[0].click();", target)
+while True:
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+    time.sleep(scroll_pause_time)
+    if is_target_element_present(driver):
+        break
+
+
 time.sleep(10)
 driver.quit()
