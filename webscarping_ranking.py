329a2a691bc67b05e7ceeac9b8c0df5ae2019be2
Thamer Sekhri <95574184+T2ABIZZY@users.noreply.github.com>
2024-06-09 22:01:26 +0100

    finishing cleaning the code

--- webscarping_ranking.py
+++ webscarping_ranking.py
@@ -8,6 +8,8 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
+
+
 def setup_driver():
     chrome_options = Options()
     chrome_options.add_argument("--no-sandbox")  
@@ -16,19 +18,10 @@ def setup_driver():
     service = Service(ChromeDriverManager().install())
     driver = webdriver.Chrome(service=service, options=chrome_options)
     return driver 
-driver = setup_driver()
-driver.get("https://inside.fifa.com/fifa-world-ranking/men")
+
 def wait_for_element(driver,by,value,timeout=10):
     return WebDriverWait(driver,timeout).until(EC.element_to_be_clickable((by,value)))
-accept_button = WebDriverWait(driver, 10).until(
-        EC.element_to_be_clickable((By.ID, "onetrust-accept-btn-handler"))
-    )
-accept_button.click()
-time.sleep(3)
 
-target = WebDriverWait(driver, 10).until(
-    EC.presence_of_element_located((By.CLASS_NAME, "button-module_contentContainer__QyE5V"))
-)
 
 def scroll_to_element(driver,element,scroll_pause_time=0.5):
     while True:
@@ -38,22 +31,19 @@ def scroll_to_element(driver,element,scroll_pause_time=0.5):
             driver.execute_script("arguments[0].scrollIntoView(true);", element)
             break
 
+
 def is_target_element_present(driver):
     elements = driver.find_elements(By.CLASS_NAME, "rank-cell_rank__yNDOI")
     return any(element.text== "210" for element in elements)
     
-driver.execute_script("arguments[0].click();", target)
-while True:
-    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-    time.sleep(scroll_pause_time)
-    if is_target_element_present(driver):
-        break
+
 
 def fetch_ranking_data(driver):
     page_source = driver.page_source
     soup = BeautifulSoup(page_source, 'html.parser')
     ranks = soup.find_all('tr',
-        class_="table-row-module_row__3wRGf table-row-module_hover__MdRZU table-row-module_regular__tAYiC data-grid-module_pointer__uipYu base-world-ranking-table_tableRow__fC_zY")
+        class_="table-row-module_row__3wRGf table-row-module_hover__MdRZU table-row-module_regular__tAYiC data-grid-module_pointer__uipYu base-world-ranking-table_tableRow__fC_zY"
+        )
     full_name_ranking= [rank.find('a','link-module_link__F9IVG team-cell_teamName__tyiAD').text for rank in ranks]
     small_name_ranking= [rank.find('a','link-module_link__F9IVG team-cell_teamCode__Yi4NC').text for rank in ranks]
     total_points=[rank.find('span','total-points-cell_points__JPjv3').text for rank in ranks]
@@ -73,6 +63,15 @@ def main():
         accept_button= wait_for_element(driver,By.ID,"onetrust-accept-btn-handler")
         accept_button.click()
         time.sleep(3)
+        target_button = wait_for_element(driver,By.CLASS_NAME,"button-module_contentContainer__QyE5V")
+        scroll_to_element(driver,target_button)
+        driver.execute_script("arguments[0].click();", target_button)
+        
+        scroll_pause_time = 0.5
+        while not is_target_element_present(driver):
+            driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+            time.sleep(scroll_pause_time)
+
         full_name_ranking,small_name_ranking, total_points= fetch_ranking_data(driver)
         save_to_csv(full_name_ranking,small_name_ranking,total_points)
     except Exception as e:
