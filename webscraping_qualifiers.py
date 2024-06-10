4db019077f9c32002e6498e9a55d3584ce6d9557
Thamer Sekhri <95574184+T2ABIZZY@users.noreply.github.com>
2024-06-10 09:53:40 +0100

    adding main and setup_driver func

--- webscraping_qualifiers.py
+++ webscraping_qualifiers.py
@@ -8,3 +8,17 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
+
+def setup_driver():
+    chrome_options = Options()
+    chrome_options.add_argument("--no-sandbox")  
+    chrome_options.add_argument('--incognito')
+    chrome_options.add_argument("--disable-dev-shm-usage")  
+    service = Service(ChromeDriverManager().install())
+    driver = webdriver.Chrome(service=service, options=chrome_options)
+    return driver 
+
+def main():
+        driver = setup_driver()
+if __name__ = "__main__":
+    main()
