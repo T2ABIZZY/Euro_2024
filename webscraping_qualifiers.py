2e32f8b24b95f19312fa20dbb771d0fe1926f36f
Thamer Sekhri <95574184+T2ABIZZY@users.noreply.github.com>
2024-06-18 19:01:09 +0100

    getting page source of all files

--- webscraping_qualifiers.py
+++ webscraping_qualifiers.py
@@ -4,12 +4,23 @@ from selenium.webdriver.support.ui import Select
 import Functions
 
 def fetch_data(driver):
-    page_source = driver.page_source
-    soup = BeautifulSoup(page_source, 'html.parser')
-    matches = soup.find_all('a', class_='css-13nvyko-MatchWrapper')
-    for match in matches:
-        link = "fotmob.com" + match['href']
-        
+    try:
+
+        page_source = driver.page_source
+        soup = BeautifulSoup(page_source, 'html.parser')
+        matches = soup.find_all('a', class_='css-13nvyko-MatchWrapper')
+        for match in matches:
+            relative_link = match['href']
+            full_link = f"https://www.fotmob.com{relative_link}:tab=stats"
+            driver.get(full_link)
+            page_source = driver.page_source
+            soup = BeautifulSoup(page_source, 'html.parser')
+
+
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+
 def main():
     driver = Functions.setup_driver()
     driver.get("https://www.fotmob.com/leagues/50/matches/euro/by-round")
@@ -17,6 +28,7 @@ def main():
         which_round = Select(driver.find_element(By.CLASS_NAME, "css-1o54t4m-Select-applyMediumHover"))
         which_round.select_by_visible_text("Round 1")
         fetch_data(driver)
+        print(a)
     except Exception as e:
         print(f"an error occurred {e}")
     finally:
