f20fae1383bedad0b8bffabd9f826fea05bd0df9
Thamer Sekhri <95574184+T2ABIZZY@users.noreply.github.com>
2024-06-18 11:34:31 +0100

    selecting round

--- webscraping_qualifiers.py
+++ webscraping_qualifiers.py
@@ -1,13 +1,17 @@
+import select
+
 from bs4 import BeautifulSoup
 import time
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select
 import Functions
 
 def main():
     driver = Functions.setup_driver()
     driver.get("https://www.fotmob.com/leagues/50/matches/euro/by-round")
     try:
-        time.sleep(5)
+        which_round = Select(driver.find_element(By.CLASS_NAME, "css-1o54t4m-Select-applyMediumHover"))
+        which_round.select_by_visible_text("Round 1")
 
     except Exception as e:
         print(f"an error occurred {e}")
