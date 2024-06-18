dc921623d79703f4cbfc129c2fbb06a3827190b1
Thamer Sekhri <95574184+T2ABIZZY@users.noreply.github.com>
2024-06-18 11:57:40 +0100

    fetching all matches and fetching links

--- webscraping_qualifiers.py
+++ webscraping_qualifiers.py
@@ -6,13 +6,17 @@ import Functions
 def fetch_data(driver):
     page_source = driver.page_source
     soup = BeautifulSoup(page_source, 'html.parser')
+    matches = soup.find_all('a', class_='css-13nvyko-MatchWrapper')
+    for match in matches:
+        link = "fotmob.com" + match['href']
+        
 def main():
     driver = Functions.setup_driver()
     driver.get("https://www.fotmob.com/leagues/50/matches/euro/by-round")
     try:
         which_round = Select(driver.find_element(By.CLASS_NAME, "css-1o54t4m-Select-applyMediumHover"))
         which_round.select_by_visible_text("Round 1")
-
+        fetch_data(driver)
     except Exception as e:
         print(f"an error occurred {e}")
     finally:
