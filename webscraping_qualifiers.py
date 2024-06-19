fc7efbb1d9cfde990bd368a0a5825f89b350fd5d
Thamer Sekhri <95574184+T2ABIZZY@users.noreply.github.com>
2024-06-19 20:50:19 +0100

    round1-round2 all matches

--- webscraping_qualifiers.py
+++ webscraping_qualifiers.py
@@ -1,32 +1,39 @@
 from bs4 import BeautifulSoup
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
+from selenium.webdriver.support.wait import WebDriverWait
+import time
 import Functions
 
 def fetch_data(driver):
     try:
-
         page_source = driver.page_source
         soup = BeautifulSoup(page_source, 'html.parser')
-        matches = soup.select('div.slick-slide.slick-active.slick-current a.css-13nvyko-MatchWrapper')
+        matches = soup.select('div.slick-slide.slick-active a.css-13nvyko-MatchWrapper')
+        print(matches)
         for match in matches:
             relative_link = match['href']
             full_link = f"https://www.fotmob.com{relative_link}:tab=stats"
             driver.get(full_link)
             page_source = driver.page_source
             soup = BeautifulSoup(page_source, 'html.parser')
-
-
     except Exception as e:
         print(f"An error occurred: {e}")
 
 
 def main():
+    original_link = "https://www.fotmob.com/leagues/50/matches/euro/by-round"
     driver = Functions.setup_driver()
-    driver.get("https://www.fotmob.com/leagues/50/matches/euro/by-round")
+    driver.get(original_link)
     try:
         which_round = Select(driver.find_element(By.CLASS_NAME, "css-b08zi1-Select-applyMediumHover"))
         which_round.select_by_visible_text("Round 1")
+        fetch_data(driver)
+        driver = Functions.setup_driver()
+        driver.get(original_link)
+
+        which_round = Select(driver.find_element(By.CLASS_NAME, "css-b08zi1-Select-applyMediumHover"))
+        which_round.select_by_visible_text("Round 2")
         fetch_data(driver)
     except Exception as e:
         print(f"an error occurred {e}")
