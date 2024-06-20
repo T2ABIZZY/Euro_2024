3ebd73115e0dacbb07e780155bd14dd0c54e175a
Thamer Sekhri <95574184+T2ABIZZY@users.noreply.github.com>
2024-06-20 20:22:02 +0100

    fixing classes names

--- webscraping_qualifiers.py
+++ webscraping_qualifiers.py
@@ -4,12 +4,13 @@ from selenium.webdriver.support.ui import Select
 from selenium.webdriver.support.wait import WebDriverWait
 import time
 import Functions
-
-def fetch_data(driver):
+def fetch_data(soup):
+    print("test")
+def fetch_matches(driver):
     try:
         page_source = driver.page_source
         soup = BeautifulSoup(page_source, 'html.parser')
-        matches = soup.select('div.slick-slide.slick-active a.css-13nvyko-MatchWrapper')
+        matches = soup.select('div.slick-slide.slick-active a.css-9on8f0-MatchWrapper')
         print(matches)
         for match in matches:
             relative_link = match['href']
@@ -17,6 +18,7 @@ def fetch_data(driver):
             driver.get(full_link)
             page_source = driver.page_source
             soup = BeautifulSoup(page_source, 'html.parser')
+            fetch_data(soup)
     except Exception as e:
         print(f"An error occurred: {e}")
 
@@ -27,15 +29,15 @@ def main():
     driver.get(original_link)
     try:
         # Round 1
-        which_round = Select(driver.find_element(By.CLASS_NAME, "css-b08zi1-Select-applyMediumHover"))
+        which_round = Select(driver.find_element(By.CLASS_NAME, "css-9n0p7i-Select"))
         which_round.select_by_visible_text("Round 1")
-        fetch_data(driver)
+        fetch_matches(driver)
         #Round 2
         driver = Functions.setup_driver()
         driver.get(original_link)
-        which_round = Select(driver.find_element(By.CLASS_NAME, "css-b08zi1-Select-applyMediumHover"))
+        which_round = Select(driver.find_element(By.CLASS_NAME, "css-9n0p7i-Select"))
         which_round.select_by_visible_text("Round 2")
-        fetch_data(driver)
+        fetch_matches(driver)
     except Exception as e:
         print(f"an error occurred {e}")
     finally:
