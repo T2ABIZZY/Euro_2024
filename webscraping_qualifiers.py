1360a5e0db49036487a02c98556067f24089d54e
Thamer Sekhri <95574184+T2ABIZZY@users.noreply.github.com>
2024-07-06 19:08:08 +0100

    extracting all data

--- webscraping_qualifiers.py
+++ webscraping_qualifiers.py
@@ -9,20 +9,41 @@ def clean_text(text):
     text = re.sub(r'[^a-zA-Z0-9\s]', '', text)
     text = text.strip()
     return text
+data={
+    'stadium':[],
+    'attendance':[],
+    'home_team':[],
+    'away_team':[],
+    'home_goals':[],
+    'away_goals':[]
+}
+rank_headers=[
+    'Expected goals (xG)','Total shots','Shots on target','Big chances',
+    'Big chances missed','Accurate passes','Fouls committed','Corners',
+    'Total shots','Shots off target','Shots on target','Blocked shots'
+    'Hit woodwork','Shots inside box','Shots outside box','Expected goals (xG)'
+    'xG open play','xG set play','Non-penalty xG','xG on target (xGOT)'
+    'Passes','Accurate passes','Own half','Opposition half'
+    'Accurate long balls','Accurate crosses','Throws','Touches in opposition box'
+    'Offsides','Tackles won','Interceptions','Blocks'
+    'Clearances','Keeper saves','Yellow cards','Red cards'
+    'Duels won','Ground duels won','Aerial duels won','Successful dribbles'
+]
+rank_data = {header: [] for header in rank_headers}
 def fetch_data(soup):
     header = soup.find('div', 'css-1pf15hj-MFHeaderInfoBoxCSS')
-    date =[header.find('div','css-1tttqnj-MatchDateCSS').time['datetime']]
-    stadium = [clean_text(header.find('a','css-ndn9i5-VenueCSS').span.text)]
-    attendance = [header.find('div', 'css-1r6wxia-AttendanceCSS').span.text]
+    data['stadium'].append(clean_text(header.find('a','css-ndn9i5-VenueCSS').span.text))
+    data['attendance'].append(header.find('div', 'css-1r6wxia-AttendanceCSS').span.text)
     main_board = soup.find('section','css-154n3ly-MFHeaderFullscreenSection')
-    home_team = [main_board.find('div', 'css-1o6li2i-TeamMarkup').find('span',
-                               'css-dpbuul-TeamNameItself-TeamNameOnTabletUp').text]
-    away_team = [main_board.find('div', 'css-6p9nys-TeamMarkup').find('span',
-                               'css-dpbuul-TeamNameItself-TeamNameOnTabletUp').text]
+    data['home_team'].append(main_board.find('div', 'css-1o6li2i-TeamMarkup').find('span',
+                               'css-dpbuul-TeamNameItself-TeamNameOnTabletUp').text)
+    data['away_team'].append(main_board.find('div', 'css-6p9nys-TeamMarkup').find('span',
+                               'css-dpbuul-TeamNameItself-TeamNameOnTabletUp').text)
     score = main_board.find('span', 'css-ktw5ic-MFHeaderStatusScore').text
-    home_goals = [score[0]]
-    away_goals = [score[-1]]
-    return date,stadium,attendance,home_team,away_team
+    data['home_goals'].append(score[0])
+    data['away_goals'].append(score[-1])
+    ranks = soup.find_all('div', class_='css-136hnlq-StatBox')
+
 def fetch_matches(driver):
     try:
         page_source = driver.page_source
@@ -45,15 +66,16 @@ def main():
     driver.get(original_link)
     try:
         # Round 1
-        which_round = Select(driver.find_element(By.CLASS_NAME, "css-f033xm-Select"))
+        which_round = Select(driver.find_element(By.CLASS_NAME, "css-1ugcbd1-Select"))
         which_round.select_by_visible_text("Round 1")
         fetch_matches(driver)
         #Round 2
         driver = Functions.setup_driver()
         driver.get(original_link)
-        which_round = Select(driver.find_element(By.CLASS_NAME, "css-f033xm-Select"))
+        which_round = Select(driver.find_element(By.CLASS_NAME, "css-1ugcbd1-Select"))
         which_round.select_by_visible_text("Round 2")
         fetch_matches(driver)
+        print(len(data['home_team']))
     except Exception as e:
         print(f"an error occurred {e}")
     finally:
