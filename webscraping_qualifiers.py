a09586761b5a4878bfa1d55464b879e389b15387
Thamer Sekhri <95574184+T2ABIZZY@users.noreply.github.com>
2024-07-09 17:32:37 +0100

    adding away stats

--- webscraping_qualifiers.py
+++ webscraping_qualifiers.py
@@ -19,19 +19,34 @@ data={
     'away_goals':[]
 }
 
-rank_headers=[
-    'Expected goals (xG)','Total shots','Shots on target','Big chances',
-    'Big chances missed','Accurate passes','Fouls committed','Corners',
-    'Total shots','Shots off target','Shots on target','Blocked shots',
-    'Hit woodwork','Shots inside box','Shots outside box','Expected goals (xG)',
-    'xG open play','xG set play','Non-penalty xG','xG on target (xGOT)',
-    'Passes','Accurate passes','Own half','Opposition half',
-    'Accurate long balls','Accurate crosses','Throws','Touches in opposition box',
-    'Offsides','Tackles won','Interceptions','Blocks',
-    'Clearances','Keeper saves','Yellow cards','Red cards',
-    'Duels won','Ground duels won','Aerial duels won','Successful dribbles'
+home_rank_headers = [
+    'Expected goals (xG)', 'Total shots', 'Shots on target', 'Big chances',
+    'Big chances missed', 'Accurate passes', 'Fouls committed', 'Corners',
+    'Total shots', 'Shots off target', 'Shots on target', 'Blocked shots',
+    'Hit woodwork', 'Shots inside box', 'Shots outside box', 'Expected goals (xG)',
+    'xG open play', 'xG set play', 'Non-penalty xG', 'xG on target (xGOT)',
+    'Passes', 'Accurate passes', 'Own half', 'Opposition half',
+    'Accurate long balls', 'Accurate crosses', 'Throws', 'Touches in opposition box',
+    'Offsides', 'Tackles won', 'Interceptions', 'Blocks',
+    'Clearances', 'Keeper saves', 'Yellow cards', 'Red cards',
+    'Duels won', 'Ground duels won', 'Aerial duels won', 'Successful dribbles'
 ]
-rank_dict = {header: [] for header in rank_headers}
+
+away_rank_headers = [
+    'Expected goals (xG)', 'Total shots', 'Shots on target', 'Big chances',
+    'Big chances missed', 'Accurate passes', 'Fouls committed', 'Corners',
+    'Total shots', 'Shots off target', 'Shots on target', 'Blocked shots',
+    'Hit woodwork', 'Shots inside box', 'Shots outside box', 'Expected goals (xG)',
+    'xG open play', 'xG set play', 'Non-penalty xG', 'xG on target (xGOT)',
+    'Passes', 'Accurate passes', 'Own half', 'Opposition half',
+    'Accurate long balls', 'Accurate crosses', 'Throws', 'Touches in opposition box',
+    'Offsides', 'Tackles won', 'Interceptions', 'Blocks',
+    'Clearances', 'Keeper saves', 'Yellow cards', 'Red cards',
+    'Duels won', 'Ground duels won', 'Aerial duels won', 'Successful dribbles'
+]
+home_rank_dict = {header: [] for header in home_rank_headers}
+away_rank_dict = {header: [] for header in away_rank_headers}
+
 def fetch_data(soup):
     header = soup.find('div', 'css-1pf15hj-MFHeaderInfoBoxCSS')
     data['stadium'].append(clean_text(header.find('a','css-ndn9i5-VenueCSS').span.text))
@@ -44,16 +59,20 @@ def fetch_data(soup):
     score = main_board.find('span', 'css-ktw5ic-MFHeaderStatusScore').text
     data['home_goals'].append(score[0])
     data['away_goals'].append(score[-1])
-
-    ranks = soup.find_all('div', class_='css-136hnlq-StatBox')
-
-    if len(ranks) == len(rank_headers):
-        for header, rank in zip(rank_headers, ranks):
-            rank_dict[header].append(rank.get_text(strip=True))
-    else:
-        print("Error: The number of ranks does not match the number of headers.")
-    print(len(ranks))
-    return data,rank_dict
+    try:
+        home_ranks = soup.find_all('div', class_='css-136hnlq-StatBox')
+        away_ranks = soup.find_all('div',class_='css-1hkvumm-StatBox')
+        if len(home_ranks) == len(home_rank_headers):
+            for header, rank in zip(home_rank_headers, home_ranks):
+                home_rank_dict[header].append(rank.get_text(strip=True))
+            for header, rank in zip(away_rank_headers, away_ranks):
+                away_rank_dict[header].append(rank.get_text(strip=True))
+        else:
+            print("Error: The number of home_ranks does not match the number of headers.")
+        print(len(home_ranks))
+    except Exception as e:
+        print(f"An error occurredss: {e}")
+    return data,home_rank_dict,away_rank_dict
 
 def fetch_matches(driver):
     try:
@@ -66,51 +85,84 @@ def fetch_matches(driver):
             driver.get(full_link)
             element_present = EC.presence_of_all_elements_located((By.CLASS_NAME, 'css-136hnlq-StatBox'))
             WebDriverWait(driver, 10).until(element_present)
+            element_present = EC.presence_of_all_elements_located((By.CLASS_NAME, 'css-1hkvumm-StatBox'))
+            WebDriverWait(driver, 10).until(element_present)
             page_source = driver.page_source
             soup = BeautifulSoup(page_source, 'html.parser')
             fetch_data(soup)
-
     except Exception as e:
-        print(f"An error occurred: {e}")
+        print(f"An error occurreds: {e}")
 
-def save_to_csv(data,rank_dict):
+def save_to_csv(data,home_rank_dict,away_rank_dict):
     with open("Euro_2024_Matches.csv",'w',newline='') as file:
         wr = csv.writer(file)
         wr.writerow(["stadium","attendance","home_team",'away_team','home_goals','away_goals',
-    'Expected goals (xG)','Total shots','Shots on target','Big chances',
-    'Big chances missed','Accurate passes','Fouls committed','Corners',
-    'Total shots','Shots off target','Shots on target','Blocked shots',
-    'Hit woodwork','Shots inside box','Shots outside box','Expected goals (xG)',
-    'xG open play','xG set play','Non-penalty xG','xG on target (xGOT)',
-    'Passes','Accurate passes','Own half','Opposition half',
-    'Accurate long balls','Accurate crosses','Throws','Touches in opposition box',
-    'Offsides','Tackles won','Interceptions','Blocks',
-    'Clearances','Keeper saves','Yellow cards','Red cards',
-    'Duels won','Ground duels won','Aerial duels won','Successful dribbles'])
+    'Home Expected goals (xG)','Home Total shots','Home Shots on target','Home Big chances',
+    'Home Big chances missed','Home Accurate passes','Home Fouls committed','Home Corners',
+    'Home Total shots','Home Shots off target','Home Shots on target','Home Blocked shots',
+    'Home Hit woodwork','Home Shots inside box','Home Shots outside box','Home Expected goals (xG)',
+    'Home xG open play','Home xG set play','Home Non-penalty xG','Home xG on target (xGOT)',
+    'Home Passes','Home Accurate passes','Home Own half','Home Opposition half',
+    'Home Accurate long balls','Home Accurate crosses','Home Throws','Home Touches in opposition box',
+    'Home Offsides','Home Tackles won','Home Interceptions','Home Blocks',
+    'Home Clearances','Home Keeper saves','Home Yellow cards','Home Red cards',
+    'Home Duels won','Home Ground duels won','Home Aerial duels won','Home Successful dribbles',
+    'Away Expected goals (xG)', 'Away Total shots', 'Away Shots on target', 'Away Big chances',
+    'Away Big chances missed', 'Away Accurate passes', 'Away Fouls committed', 'Away Corners',
+    'Away Total shots', 'Away Shots off target', 'Away Shots on target', 'Away Blocked shots',
+    'Away Hit woodwork', 'Away Shots inside box', 'Away Shots outside box', 'Away Expected goals (xG)',
+    'Away xG open play', 'Away xG set play', 'Away Non-penalty xG', 'Away xG on target (xGOT)',
+    'Away Passes', 'Away Accurate passes', 'Away Own half', 'Away Opposition half',
+    'Away Accurate long balls', 'Away Accurate crosses', 'Away Throws',
+    'Away Touches in opposition box',
+    'Away Offsides', 'Away Tackles won', 'Away Interceptions', 'Away Blocks',
+    'Away Clearances', 'Away Keeper saves', 'Away Yellow cards', 'Away Red cards',
+    'Away Duels won', 'Away Ground duels won', 'Away Aerial duels won', 'Away Successful dribbles'
+                     ])
         for i in range(len(data['stadium'])):
             row = [
                 data['stadium'][i], data['attendance'][i], data['home_team'][i], data['away_team'][i],
                 data['home_goals'][i], data['away_goals'][i],
-                rank_dict['Expected goals (xG)'][i], rank_dict['Total shots'][i], rank_dict['Shots on target'][i],
-                rank_dict['Big chances'][i],
-                rank_dict['Big chances missed'][i], rank_dict['Accurate passes'][i], rank_dict['Fouls committed'][i],
-                rank_dict['Corners'][i],
-                rank_dict['Total shots'][i], rank_dict['Shots off target'][i], rank_dict['Shots on target'][i],
-                rank_dict['Blocked shots'][i],
-                rank_dict['Hit woodwork'][i], rank_dict['Shots inside box'][i], rank_dict['Shots outside box'][i],
-                rank_dict['Expected goals (xG)'][i],
-                rank_dict['xG open play'][i], rank_dict['xG set play'][i], rank_dict['Non-penalty xG'][i],
-                rank_dict['xG on target (xGOT)'][i],
-                rank_dict['Passes'][i], rank_dict['Accurate passes'][i], rank_dict['Own half'][i],
-                rank_dict['Opposition half'][i],
-                rank_dict['Accurate long balls'][i], rank_dict['Accurate crosses'][i], rank_dict['Throws'][i],
-                rank_dict['Touches in opposition box'][i],
-                rank_dict['Offsides'][i], rank_dict['Tackles won'][i], rank_dict['Interceptions'][i],
-                rank_dict['Blocks'][i],
-                rank_dict['Clearances'][i], rank_dict['Keeper saves'][i], rank_dict['Yellow cards'][i],
-                rank_dict['Red cards'][i],
-                rank_dict['Duels won'][i], rank_dict['Ground duels won'][i], rank_dict['Aerial duels won'][i],
-                rank_dict['Successful dribbles'][i]
+                home_rank_dict['Expected goals (xG)'][i], home_rank_dict['Total shots'][i], home_rank_dict['Shots on target'][i],
+                home_rank_dict['Big chances'][i],
+                home_rank_dict['Big chances missed'][i], home_rank_dict['Accurate passes'][i], home_rank_dict['Fouls committed'][i],
+                home_rank_dict['Corners'][i],
+                home_rank_dict['Total shots'][i], home_rank_dict['Shots off target'][i], home_rank_dict['Shots on target'][i],
+                home_rank_dict['Blocked shots'][i],
+                home_rank_dict['Hit woodwork'][i], home_rank_dict['Shots inside box'][i], home_rank_dict['Shots outside box'][i],
+                home_rank_dict['Expected goals (xG)'][i],
+                home_rank_dict['xG open play'][i], home_rank_dict['xG set play'][i], home_rank_dict['Non-penalty xG'][i],
+                home_rank_dict['xG on target (xGOT)'][i],
+                home_rank_dict['Passes'][i], home_rank_dict['Accurate passes'][i], home_rank_dict['Own half'][i],
+                home_rank_dict['Opposition half'][i],
+                home_rank_dict['Accurate long balls'][i], home_rank_dict['Accurate crosses'][i], home_rank_dict['Throws'][i],
+                home_rank_dict['Touches in opposition box'][i],
+                home_rank_dict['Offsides'][i], home_rank_dict['Tackles won'][i], home_rank_dict['Interceptions'][i],
+                home_rank_dict['Blocks'][i],
+                home_rank_dict['Clearances'][i], home_rank_dict['Keeper saves'][i], home_rank_dict['Yellow cards'][i],
+                home_rank_dict['Red cards'][i],
+                home_rank_dict['Duels won'][i], home_rank_dict['Ground duels won'][i], home_rank_dict['Aerial duels won'][i],
+                home_rank_dict['Successful dribbles'][i],away_rank_dict['Expected goals (xG)'][i], away_rank_dict['Total shots'][i], away_rank_dict['Shots on target'][i],
+                away_rank_dict['Big chances'][i],
+                away_rank_dict['Big chances missed'][i], away_rank_dict['Accurate passes'][i], away_rank_dict['Fouls committed'][i],
+                away_rank_dict['Corners'][i],
+                away_rank_dict['Total shots'][i], away_rank_dict['Shots off target'][i], away_rank_dict['Shots on target'][i],
+                away_rank_dict['Blocked shots'][i],
+                away_rank_dict['Hit woodwork'][i], away_rank_dict['Shots inside box'][i], away_rank_dict['Shots outside box'][i],
+                away_rank_dict['Expected goals (xG)'][i],
+                away_rank_dict['xG open play'][i], away_rank_dict['xG set play'][i], away_rank_dict['Non-penalty xG'][i],
+                away_rank_dict['xG on target (xGOT)'][i],
+                away_rank_dict['Passes'][i], away_rank_dict['Accurate passes'][i], away_rank_dict['Own half'][i],
+                away_rank_dict['Opposition half'][i],
+                away_rank_dict['Accurate long balls'][i], away_rank_dict['Accurate crosses'][i], away_rank_dict['Throws'][i],
+                away_rank_dict['Touches in opposition box'][i],
+                away_rank_dict['Offsides'][i], away_rank_dict['Tackles won'][i], away_rank_dict['Interceptions'][i],
+                away_rank_dict['Blocks'][i],
+                away_rank_dict['Clearances'][i], away_rank_dict['Keeper saves'][i], away_rank_dict['Yellow cards'][i],
+                away_rank_dict['Red cards'][i],
+                away_rank_dict['Duels won'][i], away_rank_dict['Ground duels won'][i], away_rank_dict['Aerial duels won'][i],
+                away_rank_dict['Successful dribbles'][i]
+
             ]
             wr.writerow(row)
 def main():
@@ -123,14 +175,12 @@ def main():
         which_round.select_by_visible_text("Round 1")
         fetch_matches(driver)
         #Round 2
-        driver = Functions.setup_driver()
-        driver.get(original_link)
-        which_round = Select(driver.find_element(By.CLASS_NAME, "css-gk6f5k-Select"))
-        which_round.select_by_visible_text("Round 2")
-        fetch_matches(driver)
-        print(rank_dict['Expected goals (xG)'])
-        print(data['home_team'])
-        save_to_csv(data,rank_dict)
+        # driver = Functions.setup_driver()
+        # driver.get(original_link)
+        # which_round = Select(driver.find_element(By.CLASS_NAME, "css-gk6f5k-Select"))
+        # which_round.select_by_visible_text("Round 2")
+        # fetch_matches(driver)
+        save_to_csv(data,home_rank_dict,away_rank_dict)
     except Exception as e:
         print(f"an error occurred {e}")
     finally:
